aff55be5b1a9b7c8f3138000aa4869f33ea11c63
Rodolphe Le Riche <leriche@emse.fr>
2017-10-08 21:49:58 +0200

    rodo cost function done

--- TestCase/mainInversionPunctualDisplSource.R
+++ TestCase/mainInversionPunctualDisplSource.R
@@ -14,6 +14,7 @@ rm(list=ls()) #  cleaning up
 
 library(R.matlab)
 source("./mogi_3D.R")
+source("./wls_ulos.R")
 source("../labSessions/kernels.R")
 
 ####### input for variables identification ###########
@@ -33,29 +34,47 @@ xmag[n2i$xs] <- 367000 # X location of source in UTM coordinates
 xmag[n2i$ys] <- 7650300 # Y location of source in UTM
 xmag[n2i$zs] <- 1000 # Elevation of source with respect to sea level
 xmag[n2i$a] <- 500 # source radius
-xmag[n2i$p] <- 20 # Source overpressure in MPa
+xmag[n2i$p] <- 100 # Source overpressure in MPa
 # initial point (for an identification)
 xinit<-xstar+c(0.2,-0.2,-0.3,0.5,-0.4)*xmag
 # bounds on variables
-xmax <- xstar*rep(2,times=nbvar)
-  
-zvar <- list(n2i=n2i, x=xinit)
+xmax <- NA
+xmin <- NA
+xmin[n2i$xs]<-364000
+xmax[n2i$xs]<-366000
+xmin[n2i$ys]<-7649000
+xmax[n2i$ys]<-7651000
+xmin[n2i$zs]<- -3000
+xmax[n2i$zs]<-2000
+xmin[n2i$a]<- 50
+xmax[n2i$a]<- 2000
+xmin[n2i$p]<- -500
+xmax[n2i$p]<-500
+#
 
+
+Glb_var <<- list(n2i=n2i,nbvar=nbvar,xmag=xmag,xmax=xmax,xmin=xmin) # always useful stuff
 
 ####### load data ###########
-data <- readMat('data_nonoise.mat')
-Glb_xi <- data$locdata[,1]
-Glb_yi <- data$locdata[,2]
-Glb_zi <- data$locdata[,3]
-Glb_ulos <- data$locdata[,4]
+data <- readMat('data_nonoise.mat') # TODO: do a read from cvs version (rodo)
+Glb_xi <<- as.matrix(data$locdata[,1])
+Glb_yi <<- as.matrix(data$locdata[,2])
+Glb_zi <<- as.matrix(data$locdata[,3])
+Glb_ulos <<- as.matrix(data$locdata[,4])
 
 # calculate data Covariance matrix, store it in a Global variable
 # covariance from exponential kernel, var = 5e-4m2, cor_length = 850 m
+# and invert it
 Xdata <- data$locdata[,1:2] # z's are not accounted for in Xdata
-Glb_CovXdata <<- kExp(Xdata,Xdata,c(5e-4,850,850)) 
+Glb_CXinv <<- solve(kExp(Xdata,Xdata,c(5e-4,850,850))) 
 # # has been compared to the matlab covariance matrix below, max difference 1e-6
 # data <- readMat('cov_nonoise.mat')
 # CovData <- data$Cd
 rm(data)
 rm(Xdata)
 
+# a sample call to the distance function would look like this:
+# zvar <- list(n2i=n2i, x=xinit)
+# wls <- wls_ulos(zvar$x)
+
+######  do a design of experiments #############################
